d297433107e942c8f68f96e9da4c3480678f64dc
webwlx <web_wlx@163.com>
2022-10-15 18:06:16 +0800

    引入u-view 组件库

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="list-container">
+			
 		</view>
 	</view>
 </template>
